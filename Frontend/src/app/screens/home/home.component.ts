90991d2f4f3deafc1f88aeeafe4287fea3c13cd4
Abubakar Mughal <64458681+abubakar2000@users.noreply.github.com>
2022-04-22 23:50:57 +0500

    home

--- Frontend/src/app/screens/home/home.component.ts
+++ Frontend/src/app/screens/home/home.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import '../../../assets/css/bootstrap.min.css'
-import '../../../assets/css/font-awesome.css'
-import '../../../assets/css/templatemo-art-factory.css'
-import '../../../assets/css/owl-carousel.css'
+// import '../../../assets/js/jquery-2.1.0.min.js'
+// import '../../../assets/js/popper.js'
+// import '../../../assets/js/owl-carousel.js'
+// import '../../../assets/js/scrollreveal.min.js'
+// // import '../../../assets/js/waypoints.min.js'
+// import '../../../assets/js/jquery.counterup.min.js'
+// import '../../../assets/js/imgfix.min.js'
+// import '../../../assets/js/custom.js'
+
 
 @Component({
   selector: 'app-home',
